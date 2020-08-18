dd1ccb78a6cbfd6b532a126ed3571ad5d6013047
wangshixiang <wangshixiang1020@gmail.com>
2020-08-18 08:35:41 +0800

    增加空数据返回格式

--- src/redis/reply/common.go
+++ src/redis/reply/common.go
@@ -3,6 +3,12 @@ package reply
 type OkReply struct {
 }
 
+type NullBulkReply struct {
+}
+
 func (r *OkReply) ToBytes() []byte {
 	return []byte("+OK\r\n")
+}
+func (r *NullBulkReply) ToBytes() []byte {
+	return nullBulkReplyBytes
 }
